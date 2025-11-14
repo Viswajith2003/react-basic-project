2a299e264a034e10a853efa51c4ed3d1b58b9e79
Viswajith2003 <viswajithviswa715@gmail.com>
2025-11-15 04:02:10 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Hello from "./components/hello";
+
+function App() {
+  return (
+    <div className="">
+      <Hello />
+      <br />
+      <h1 className="text-4xl font-bold text-blue-600">Hello Tailwind CSS!</h1>{" "}
+    </div>
+  );
+}
+
+export default App;
